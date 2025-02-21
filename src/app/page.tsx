197becedf79169029d550c94472fab8d1d18e36d
Glenardd <tolentinojanglenard5@gmail.com>
2025-02-21 13:57:12 +0800

    Remove dashboard page component and integrate dashboard text into home page; update navbar for logo linking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <>
       <div>
         <Navbar />
+        <div>Dashboard</div>
       </div>
     </>
   );
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,31 +1,34 @@
-import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs"; 
+import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 export default function navbar() {
-  return (
-    <nav className="flex items-center justify-between flex-wrap bg-white p-4 shadow-md">
-        <div className="flex items-center flex-shrink-0 text-black mr-6 gap-2">
-            <Image src="/PHO.png" width={50} height={50} alt="PHO Logo"/>
-            <span>Provincial Health Office Scheduler</span>
-        </div>
-        <div className="flex items-center flex-shrink-0 text-black mr-6">
-            <SignedIn>
-                <UserButton appearance={{
-                    elements: {
-                        avatarBox:{
-                            width: 40,
-                            height: 40
-                        },
-                    }}
-                }/>
-            </SignedIn>
-            <SignedOut>
-                <SignInButton>
-                    <Button>Sign In</Button>
-                </SignInButton>
-            </SignedOut>
-        </div>
-    </nav>
-  )
+    return (
+        <nav className="flex items-center justify-between flex-wrap bg-white p-4 shadow-md">
+            <div className="flex items-center flex-shrink-0 text-black mr-6 gap-2">
+                <Link href="/" passHref>
+                    <Image src="/PHO.png" width={50} height={50} alt="PHO Logo" />
+                </Link>
+                <span>Provincial Health Office Scheduler</span>
+            </div>
+            <div className="flex items-center flex-shrink-0 text-black mr-6">
+                <SignedIn>
+                    <UserButton appearance={{
+                        elements: {
+                            avatarBox: {
+                                width: 40,
+                                height: 40
+                            },
+                        }
+                    }} />
+                </SignedIn>
+                <SignedOut>
+                    <SignInButton>
+                        <Button>Sign In</Button>
+                    </SignInButton>
+                </SignedOut>
+            </div>
+        </nav>
+    )
 }
