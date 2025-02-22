a143842d7aad5ce69fd8ac93e85cb90165972888
Glenardd <tolentinojanglenard5@gmail.com>
2025-02-22 19:16:23 +0800

    Update navbar component to remove Link wrapper and add additional logo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,17 +2,15 @@ import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { SidebarTrigger } from "./ui/sidebar";
-import Link from "next/link";
 
 export default function navbar() {
     return (
         <nav className="flex items-center justify-between flex-wrap bg-white p-4 shadow-md">
+            <SidebarTrigger/>
             <div className="flex items-center flex-shrink-0 text-black mr-6 gap-2">
-                <SidebarTrigger/>
-                <Link href="/" passHref>
-                    <Image src="/PHO.png" width={50} height={50} alt="PHO Logo" />
-                </Link>
+                <Image src="/PHO.png" width={50} height={50} alt="PHO-Logo" />
                 <span>Provincial Health Office Scheduler</span>
+                <Image src="/PGP.png" width={50} height={50} alt="PGP-Logo" />
             </div>
             <div className="flex items-center flex-shrink-0 text-black mr-6">
                 <SignedIn>
