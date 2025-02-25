f0b20e8962b8ef10cb8b77202cbe77e6cdf94ef9
Glenardd <tolentinojanglenard5@gmail.com>
2025-02-25 16:22:01 +0800

    Add date formatting and delete button to Google API event display

--- src/components/googleApi.tsx
+++ src/components/googleApi.tsx
@@ -9,8 +9,9 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 
-
+import { SignedIn } from "@clerk/nextjs";
 import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
 
 export default function googleApi() {
 
@@ -22,12 +23,39 @@ export default function googleApi() {
 
   const event = data?.events?.items;
 
+  //
+  const dateFormat = (StartDateTime: any) =>{
+
+    const isoDate = StartDateTime;
+
+    const date = new Date(isoDate);
+
+    const format = new Intl.DateTimeFormat("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    }).format(date);
+
+    // const time = format.split('').reduce((acc, char) => {
+    //   if (/\w/.test(char) || char === ' ') {
+    //     acc += char;
+    //   }
+    //   return acc;
+    // }, '').split(' ');
+
+    return format;
+
+  }
+
   console.log(data?.events?.items);
 
   // event?.map((data:any)=> console.log(data?.organizer?.displayName))
   
   return (
-    <>
+    <SignedIn>
       {
         event?.map((event: any, i: number)=>{
           return (
@@ -47,12 +75,18 @@ export default function googleApi() {
                     </div>
                     <div className="p-5 gap-4">
                       <CardContent>
-                        <div className="items-center">
-                          {event?.start?.dateTime}
-                        </div>
-                        <div className="items-center">
-                          {event?.start?.timeZone}
+                        <div className="grid grid-flow-col grid-rows-3 gap-4">
+                          <div className="row-span-1 text-xl">
+                            <span>{dateFormat(event?.start?.dateTime)}</span>
+                            <span>{dateFormat(event?.end?.dateTime)}</span>
+                          </div>
+                          <div className="col-span-2 row-span-2">
+                            <Button>
+                            Delete
+                            </Button>
+                          </div>
                         </div> 
+                        
                       </CardContent>
                     </div>
                 </div>
@@ -61,6 +95,6 @@ export default function googleApi() {
           )
         }).reverse()
       }
-    </>
+    </SignedIn>
   );
 }
